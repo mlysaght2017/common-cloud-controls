4c78bbfb9d2791a0a66d9cf763f3d45f1d800575
Damien J. Burks <damienjburks@Damiens-MacBook-Pro.local>
2024-04-02 22:08:33 -0500

    adding in example workflow and updating package structure
    Committer: Damien J. Burks <damien.burks@citi.com>

---
+++ src/control-catalog/storage/main.tf
@@ -0,0 +1,19 @@
+module "aws_storage_object_ccc.os.c1" {
+    source = "./object/ccc.os.c1/aws"
+}
+
+module "gcp_storage_object_ccc.os.c1" {
+    source = "./object/ccc.os.c1/gcp"
+}
+
+module "aws_storage_object_ccc.os.c2" {
+    source = "./object/ccc.os.c2/aws"
+}
+
+module "gcp_storage_object_ccc.os.c2" {
+    source = "./object/ccc.os.c2/gcp"
+}
+
+module "gcp_storage_object_ccc.os.c2" {
+    source = "./object/ccc.os.c2/gcp"
+}
